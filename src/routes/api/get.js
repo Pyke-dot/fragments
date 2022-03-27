c8862c96bddf6a1ea6f5b1c6aa55054eac7b2df1
Tecca Yu <onionringsaregood@gmail.com>
2022-03-26 21:35:25 -0400

    settings modification, get.js modified setHeader, added post.test

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -23,7 +23,7 @@ module.exports = {
               let ext = fragmentId[1];
               if (ext == 'html') {
                 if (fragmentM.type == 'text/markdown') {
-                  res.set('Content-Type', 'text/html');
+                  res.setHeader('Content-Type', 'text/html');
                   var result = md.render(fragment.toString());
                   res.status(200).send(result);
                   logger.info({ targetType: ext }, `successfully convert to ${ext}`);
@@ -34,7 +34,7 @@ module.exports = {
                 }
               }
             } else {
-              res.set('Content-Type', fragmentM.type);
+              res.setHeader('Content-Type', fragmentM.type);
               res.status(200).send(fragment);
               logger.info(
                 { fragmentData: fragment, contentType: fragmentM.type },
